63136fdd1e863342db244e414db8f28260829f1e
Afsana-Tasnim-Juha <111423065+Afsana-Tasnim-Juha@users.noreply.github.com>
2023-08-25 17:12:18 +0200

    coupon added

--- scripts/sale-calculator.js
+++ scripts/sale-calculator.js
@@ -1,10 +1,19 @@
-//function handleClickBtn(target) {
-//const selectedProductsContainer = document.getElementById('selected-products')
-//const itemName = target.parentNode.childNodes[6].innerText;
-//const li = document.createElement("li");
-//li.innerText = itemName;
-//selectedProductsContainer.appendChild(li);
-//}
+
+let total = 0;
+document.getElementById('correct-coupon').addEventListener('keyup', function (event) {
+    const text = event.target.value;
+    const couponBtn = document.getElementById('btn-apply');
+
+    if (text === 'SELL200') {
+        couponBtn.removeAttribute('disabled');
+    }
+    else {
+        couponBtn.setAttribute('disabled', true);
+    }
+});
+
+
+
 
 function handleClickBtn(target) {
     const selectedProductsContainer = document.getElementById('selected-products');
@@ -13,5 +22,22 @@ function handleClickBtn(target) {
     li.innerText = itemName;
     selectedProductsContainer.appendChild(li);
 
+    const price = target.closest('.card').querySelector('.text-gray-400 span').innerText;
+    total = parseInt(total) + parseInt(price);
+    document.getElementById("total").innerText = total;
+
+
+
 
 }
+
+
+
+
+
+
+
+
+
+
+
